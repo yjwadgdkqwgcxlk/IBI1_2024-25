35e1ef0ab99b949b76c1ea02a8ff4c72bb3d02fd
刘奕彤 <yitong.24@intl.zju.edu.cn>
2025-03-14 03:26:58 +0100

    1

---
+++ Practical4/mystery_code.py
@@ -0,0 +1,21 @@
+# What does this piece of code do?
+# Answer:This code simulates rolling two dice repeatedly until a pair of matching numbers is rolled, and tracks the number of rolls it takes.
+
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0   #"progress"means the number of times the dices are throwm to make the numbers on the two dices the same.
+while progress>=0:
+	progress+=1
+	first_n = randint(1,6) # to get the number on the first dice
+	second_n = randint(1,6)  # to get the number on the second dice
+	if first_n == second_n:
+		print(progress)
+		break
+
